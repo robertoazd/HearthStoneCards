3b1c2dbb93eae4c24e3107c32f06cc3858b0660e
Roberto Azeredo <robertoazdd@gmail.com>
2023-05-17 10:55:30 -0300

    Adicionado o objeto RetrofitModule.kt e os Provides de OkHttp, Moshi, Retrofit e CardSetApi

---
+++ app/src/main/java/com/robertoazeredo/hearthstonecardsgit/di/RetrofitModule.kt
@@ -0,0 +1,72 @@
+package com.robertoazeredo.hearthstonecardsgit.di
+
+import com.robertoazeredo.hearthstonecardsgit.BuildConfig
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    private const val API_URL = BuildConfig.API_URL
+
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+
+        val interceptor = Interceptor { chain ->
+            val request = chain.request().newBuilder().run {
+                addHeader("X-RapidAPI-key" , BuildConfig.API_KEY)
+                addHeader("X-RapidAPI-Host" , BuildConfig.API_HOST)
+                build()
+            }
+            chain.proceed(request)
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        moshi: Moshi
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(API_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideCardSetApi(
+        retrofit: Retrofit
+    ): CardSetApi {
+        return retrofit.create(CardSetApi::class.java)
+    }
+}
