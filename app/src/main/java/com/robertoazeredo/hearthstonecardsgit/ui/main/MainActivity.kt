f2c204456f99f5aa37445ddb640f870470ba1050
Roberto Azeredo <robertoazdd@gmail.com>
2023-05-19 13:22:19 -0300

    Adicionado viewBinding na MainActivity.kt

--- app/src/main/java/com/robertoazeredo/hearthstonecardsgit/ui/main/MainActivity.kt
+++ app/src/main/java/com/robertoazeredo/hearthstonecardsgit/ui/main/MainActivity.kt
@@ -2,21 +2,18 @@ package com.robertoazeredo.hearthstonecardsgit.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import com.robertoazeredo.hearthstonecardsgit.R
-import com.robertoazeredo.hearthstonecardsgit.ui.viewmodel.CardsViewModel
+import android.view.LayoutInflater
+import com.robertoazeredo.hearthstonecardsgit.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: CardsViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
-        viewModel.getCards()
     }
 
 }
