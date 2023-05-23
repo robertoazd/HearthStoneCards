407acd8bc3f31e79098cf0c7a2975aadf3270f14
Roberto Azeredo <robertoazdd@gmail.com>
2023-05-23 19:56:10 -0300

    Adicionado viewModel no CardsFragment.kt

--- app/src/main/java/com/robertoazeredo/hearthstonecardsgit/ui/view/CardsFragment.kt
+++ app/src/main/java/com/robertoazeredo/hearthstonecardsgit/ui/view/CardsFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.robertoazeredo.hearthstonecardsgit.databinding.FragmentCardsBinding
+import com.robertoazeredo.hearthstonecardsgit.ui.viewmodel.CardsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CardsFragment : Fragment() {
 
     private lateinit var binding: FragmentCardsBinding
+    private val viewModel: CardsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,4 +26,12 @@ class CardsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
+        super.onViewCreated(view , savedInstanceState)
+        if (viewModel.cards.value == null) {
+            viewModel.getCards()
+        }
+    }
+
+    
 }
