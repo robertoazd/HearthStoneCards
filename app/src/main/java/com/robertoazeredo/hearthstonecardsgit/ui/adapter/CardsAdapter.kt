a9f5dde8a2ccb4f44ee4cc96a2be6d1a9772a8e7
Roberto Azeredo <robertoazdd@gmail.com>
2023-05-23 20:34:03 -0300

    Criado a classe CardsAdapter.kt

---
+++ app/src/main/java/com/robertoazeredo/hearthstonecardsgit/ui/adapter/CardsAdapter.kt
@@ -0,0 +1,46 @@
+package com.robertoazeredo.hearthstonecardsgit.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.robertoazeredo.hearthstonecardsgit.data.model.CardResponse
+import com.robertoazeredo.hearthstonecardsgit.databinding.ItemCardBinding
+
+class CardsAdapter(
+    var itemClick: (card: CardResponse) -> Unit = {}
+) : RecyclerView.Adapter<CardsAdapter.CardViewHolder>() {
+
+    private var cards: List<CardResponse> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup , viewType: Int): CardViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemCardBinding.inflate(layoutInflater, parent, false)
+        return CardViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CardViewHolder , position: Int) {
+        holder.bind(cards[position], itemClick)
+    }
+
+    override fun getItemCount() = cards.size
+
+    fun insertCards(cards: List<CardResponse>) {
+        notifyItemRangeRemoved(0, this.cards.size)
+        this.cards = cards
+        notifyItemInserted(this.cards.size)
+    }
+
+    class CardViewHolder(
+        private val binding: ItemCardBinding
+    ) : RecyclerView.ViewHolder(binding.root){
+
+        fun bind(card: CardResponse, itemClick: (card: CardResponse) -> Unit = {}) {
+            Glide.with(binding.root.context).load(card.imageCard).into(binding.imageCard)
+            binding.root.setOnClickListener{
+                itemClick.invoke(card)
+            }
+        }
+    }
+
+}
