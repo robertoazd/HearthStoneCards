65e0ecbfd259784b28a6fff5a241aa3fb0459e00
Roberto Azeredo <robertoazdd@gmail.com>
2023-05-23 21:20:19 -0300

    Adicionado args no CardDetailFragment.kt

--- app/src/main/java/com/robertoazeredo/hearthstonecardsgit/ui/view/CardDetailFragment.kt
+++ app/src/main/java/com/robertoazeredo/hearthstonecardsgit/ui/view/CardDetailFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.robertoazeredo.hearthstonecardsgit.R
 
 class CardDetailFragment : Fragment() {
+
+    private val args: CardDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
@@ -17,4 +20,8 @@ class CardDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_card_detail , container , false)
     }
 
+    override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
+        super.onViewCreated(view , savedInstanceState)
+        println(args.card)
+    }
 }
